9a76ca4eec401e24747db4cf6caf231b5b9807ea
VAmanJain <vaman.jain17@gmail.com>
2023-12-15 19:46:01 +0530

    navbar styling change

--- src/component/Navbar/navbar.jsx
+++ src/component/Navbar/navbar.jsx
@@ -1,36 +1,34 @@
-import {Link} from "react-router-dom";
+import { Link } from "react-router-dom";
 import "./navbar.css";
-function NavBar (){
-    return(
-        <>
-        <div className="nav" >
-
-        <h1 >
-            Fun-Bar
-        </h1>
-        <ul style={{position:"relative" ,top:"-100rem"}}>
-  <li><Link to="/">Home</Link></li>
-  <li><Link to="/tictac">Tic-Tac</Link></li>
-  <li><Link to="/card">Cards</Link></li>
-  <li><Link to="/rps">Rps</Link></li>
-  <li><Link to="/about">About</Link></li>
-
-
-
-</ul>
-        <ul className="" >
-  <li><Link to="/">Home</Link></li>
-  <li><Link to="/tictac">Tic-Tac</Link></li>
-  <li><Link to="/card">Cards</Link></li>
-  <li><Link to="/rps">Rps</Link></li>
-  <li><Link to="/about">About</Link></li>
-
-
-
-</ul>
+function NavBar() {
+  return (
+    <>
+      <div className="nav">
+        <div className="icon">
+          <span>FunBar</span>
         </div>
+        <div className="list">
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/tictac">Tic Tac</Link>
+            </li>
+            <li>
+              <Link to="/rps">Rps</Link>
+            </li>
+            <li>
+              <Link to="/card">Card</Link>
+            </li>
+            <li>
+              <Link to="/about">About</Link>
+            </li>
+          </ul>
+        </div>
+      </div>
     </>
-    )
+  );
 }
 
 export default NavBar;
