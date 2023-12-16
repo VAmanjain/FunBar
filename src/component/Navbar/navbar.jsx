99f8f7ec789045a6992504f069112e74941f6a41
VAmanJain <vaman.jain17@gmail.com>
2023-12-16 11:28:42 +0530

    responsive navbar

--- src/component/Navbar/navbar.jsx
+++ src/component/Navbar/navbar.jsx
@@ -1,33 +1,47 @@
 import { Link } from "react-router-dom";
+import { useState } from "react";
 import "./navbar.css";
+
+import { GiHamburgerMenu } from "react-icons/gi";
+import { RiCloseLine } from "react-icons/ri"; // Changed RxCross2 to RiCloseLine assuming it's the correct icon
+
 function NavBar() {
+  const [menu, setMenu] = useState("list");
+
   return (
-    <>
-      <div className="nav">
+    <div className="nav">
+      <div className="nav-bar">
         <div className="icon">
           <span>FunBar</span>
         </div>
-        <div className="list">
-          <ul>
-            <li>
-              <Link to="/">Home</Link>
-            </li>
-            <li>
-              <Link to="/tictac">Tic Tac</Link>
-            </li>
-            <li>
-              <Link to="/rps">Rps</Link>
-            </li>
-            <li>
-              <Link to="/card">Card</Link>
-            </li>
-            <li>
-              <Link to="/about">About</Link>
-            </li>
-          </ul>
+        <div children="menu-icon">
+          <GiHamburgerMenu
+            onClick={() => setMenu("list showList")}
+            className="openMenu"
+          />
         </div>
       </div>
-    </>
+      <div className={menu}>
+        <RiCloseLine onClick={() => setMenu("list")} className="closeMenu" />
+        <ul>
+          <li>
+            <Link to="/" onClick={()=> setMenu("list")}>Home</Link>
+          </li>
+          <li>
+            <Link to="/tictac" onClick={()=> setMenu("list")}>Tic Tac</Link>
+          </li>
+          <li>
+            <Link to="/rps" onClick={()=> setMenu("list")}>Rps</Link>
+          </li>
+          <li>
+            <Link to="/card" onClick={()=> setMenu("list")}>Card</Link>
+          </li>
+          <li>
+            <Link to="/about" onClick={()=> setMenu("list")}>About</Link>
+          </li>
+        </ul>
+      </div>
+    </div>
   );
 }
 
